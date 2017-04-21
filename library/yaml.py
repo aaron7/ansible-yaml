877d91635eeadc6feaf8378cdfbdec050ec89731
Aaron Kirkbride <aaron@aaronkirkbride.com>
2017-04-22 00:06:14 +0100

    Add initial fields

--- library/yaml.py
+++ library/yaml.py
@@ -2,9 +2,37 @@ from ansible.module_utils.basic import AnsibleModule
 
 
 def main():
-    module = AnsibleModule(argument_spec={})
-    response = {"hello": "world"}
-    module.exit_json(changed=False, meta=response)
+    fields = {
+        "path": {"requred": True, "type": "str"},
+        "key": {"required": True, "type": "str"},
+        "value": {"required": True, "type": "str"},
+        "state": {
+            "default": "present",
+            "choices": ["present", "absent"],
+            "type": "str"
+        },
+        "ordered": {"default": True, "type": "bool"},
+    }
+    choice_map = {
+      "present": yaml_value_present,
+      "absent": yaml_value_absent,
+    }
+
+    module = AnsibleModule(argument_spec=fields)
+    has_changed, result = choice_map.get(module.params['state'])(module.params)
+    module.exit_json(changed=has_changed, meta=result)
+
+
+def yaml_value_present(data):
+    has_changed = False
+    meta = {"present": "not yet implemented"}
+    return (has_changed, meta)
+
+
+def yaml_value_absent(data=None):
+    has_changed = False
+    meta = {"absent": "not yet implemented"}
+    return (has_changed, meta)
 
 
 if __name__ == '__main__':
